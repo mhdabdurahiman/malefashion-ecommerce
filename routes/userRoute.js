2d411c32fc39cb98cc9ee1bfd2d94f254edb01ee
mhdabdurahiman <abdurahmanettuveettil@gmail.com>
2024-01-20 16:12:18 +0530

    added post route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,6 +7,9 @@ const userRoute = express();
 userRoute.set('view engine','ejs')
 userRoute.set('views',path.join(__dirname, '../views/user'))
 
+const bodyParser = require('body-parser');
+userRoute.use(bodyParser.json());
+userRoute.use(bodyParser.urlencoded({extended:true}))
 
 
 userRoute.get('/',userController.loadIndex)
@@ -19,6 +22,8 @@ userRoute.get('/product',userController.loadProductDetails)
 userRoute.get('/blog',userController.loadBlog)
 userRoute.get('/checkout',userController.loadCheckout)
 userRoute.get('/contact',userController.loadContact)
+
+userRoute.post('/register',userController.doRegister)
 
 
 
