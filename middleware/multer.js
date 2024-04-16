f691b082c81823b9690c7052c0c9af53a350ce6a
mhdabdurahiman <abdurahmanettuveettil@gmail.com>
2024-04-16 20:47:15 +0530

    Backend file validation done in the multer file

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,6 +1,8 @@
 const multer = require( 'multer' )
 const path = require('path')
 
+const allowedFileExtensions = ['.jpg', '.jpeg', '.png']
+
 const storage = multer.diskStorage({
     destination : ( req, file, cb ) => {
         cb( null, path.join( __dirname, '../public/images/product-images' ))
@@ -11,5 +13,17 @@ const storage = multer.diskStorage({
     }
 })
 
+const fileFilter = (req, file, cb) => {
+    const fileExt = path.extname(file.originalname).toLowerCase();
+    if (allowedFileExtensions.includes(fileExt)) {
+        cb(null, true)
+    } else {
+        cb(new Error('Only .jpg, .jpeg, and .png files are allowed'));
+    }
+};
 
-module.exports  = multer({storage : storage})
+
+module.exports  = multer({
+    storage : storage,
+    fileFilter: fileFilter
+});
