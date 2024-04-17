3d663c5a5ec77657da326be8de48ef8029d19a7d
mhdabdurahiman <abdurahmanettuveettil@gmail.com>
2024-04-17 11:41:39 +0530

    cart item count bug fixes

--- middleware/cartItemCountMiddleware.js
+++ middleware/cartItemCountMiddleware.js
@@ -3,19 +3,21 @@ const Cart = require("../models/cartModel");
 const fetchCartCount = async (req, res, next) => {
   try {
     const userId = req.session.userId;
-    console.log("userId:", userId);
     if (!userId) {
       res.locals.cartCount = 0;
       return next();
     }
       const cart = await Cart.findOne({ userId: userId });
-      console.log("cart", cart);
+      console.log('cart', cart);
 
       let totalItems = 0;
       if (cart.items && cart.items.length > 0) {
         totalItems = cart.items.length;
         console.log(totalItems);
         res.locals.cartCount = totalItems;
+        next();
+      } else {
+        res.locals.cartCount = 0;
         next();
       }
   } catch (error) {
